733d02c4bba82ea53641c8c63d2e5dbb8d822f25
haderrenteria13 <haderrenteria13az@gmail.com>
2025-03-29 14:52:36 -0500

    🌐 Configure and update routes
    add new routes for Navbar, character, episodes and location.

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,21 +1,38 @@
 import { createBrowserRouter } from "react-router-dom"
 import NotFound from "../features/notFound/NotFound"
+import Navbar from "../components/layout/Navbar"
 import Home from "../features/home/Home"
 import Character from "../features/characters/Character"
 import CharacterDetail from "../features/characters/CharacterDetail"
+import Episodes from "../features/episodes/Episodes"
+import Locations from "../features/locations/Locations"
 
 const Router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />
-    },
-    {
-        path: "/personaje",
-        element: <Character />,
-    },
-    {
-        path: "/personaje/:id",
-        element: <CharacterDetail/>
+        element: <Navbar />,
+        children: [
+            {
+                path: "/home",
+                element: <Home/>
+            },
+            {
+                path: "/personajes",
+                element: <Character />,
+            },
+            {
+                path: "/personaje/:id",
+                element: <CharacterDetail/>
+            },
+            {
+                path: "/episodios",
+                element: <Episodes/>
+            },
+            {
+                path: "/locaciones",
+                element: <Locations/>
+            }
+        ]
     },
     {
         path: "*",
