4c8c07e4c3903a4678cf5278bf75580852751055
haderrenteria13 <haderrenteria13az@gmail.com>
2025-03-29 16:39:14 -0500

    💄 Update Navbar styles

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <main>
-      <nav className='flex p-4 justify-between'>
-        <NavLink to={"/home"}>
-          <h1>Rick and Morty</h1>
-        </NavLink>
+    return (
+        <main>
+            <nav className='flex py-5 px-44 justify-between'>
+                <NavLink to={"/home"}>
+                    <h1>Rick and Morty</h1>
+                </NavLink>
 
-        <ul className='flex gap-3'>
-          <li>
-            <NavLink to={"/personajes"}>Personajes</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/episodios"}>Episodios</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/locaciones"}>Locaciones</NavLink>
-          </li>
-        </ul>
-      </nav>
+                <ul className='flex gap-3'>
+                    <li>
+                        <NavLink to={"/personajes"}>Personajes</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={"/episodios"}>Episodios</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={"/locaciones"}>Locaciones</NavLink>
+                    </li>
+                </ul>
+            </nav>
 
-      <Outlet/>
-    </main>
-  )
+            <Outlet />
+        </main>
+    )
 }
 
 export default Navbar
