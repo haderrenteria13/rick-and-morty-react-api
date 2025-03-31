e45b74f685f6acb8ea9fef64111abca17f93524d
haderrenteria13 <haderrenteria13az@gmail.com>
2025-03-30 23:04:55 -0500

    💄 Update UI home

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Starfield from 'react-starfield'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="relative w-full h-screen">
+      <div className="absolute inset-0 -z-10">
+        <Starfield
+          starCount={1000}
+          starColor={[255, 255, 255]}
+          speedFactor={0.05}
+          backgroundColor="black"
+        />
+      </div>
+
+      <div className="flex flex-col items-center justify-center h-full text-white">
+        <h1 className="text-4xl font-bold">Bienvenido al Universo de Rick and Morty</h1>
+        <p className="mt-4 text-lg">Explora personajes, episodios y locaciones</p>
+      </div>
+    </div>
   )
 }
 
